75782f1dd3277a5fe999b2f7bf05cdeda584509a
Diego Davila <davila.foyo@gmail.com>
2019-12-06 17:25:24 -0600

    adding lzma support in utlis read_n_decompress.py

--- utils/read_n_decompress.py
+++ utils/read_n_decompress.py
@@ -30,6 +30,42 @@ for line in fd.readlines():
             print(num_bytes)
             print(e)
             exit(0)
+    # lzma
+    elif algo_bytes == "XZ":
+        try:
+            from lzma import decompress as lzma_decompress
+        except ImportError:
+            try:
+                from backports.lzma import decompress as lzma_decompress
+            except ImportError:
+                raise ImportError("Install lzma package with:\n pip install backports.lzma\nor\n conda install -c conda-forge backports.lzma\n(or just use Python >= 3.3).")
+            
+        try:
+            lzma_decompress(uncompressed_bytes)
+        except Exception, e:
+            print("Corrupted basket")
+            print(seek)
+            print(num_bytes)
+            print(e)
+            exit(0)
+         
+    # lz4
+    elif algo_bytes == "L4":
+        print("Unsupported algorithm: "+algo_bytes+" , skipping...")
+        #try:
+        #    import xxhash
+        #except ImportError:
+        #    raise ImportError("Install xxhash package with:\n    pip install xxhash\nor\n    conda install -c conda-forge python-xxhash")
+        #compression = self.compression.copy(uproot.const.kLZ4)
+        #compressedbytes -= 8
+        #checksum = cursor.field(self._compressed, self._format_field0)
+        #copy_cursor = copy(cursor)
+        #after_compressed = copy_cursor.bytes(self._compressed, compressedbytes)
+        #if xxhash.xxh64(after_compressed).intdigest() != checksum:
+        #    raise ValueError("LZ4 checksum didn't match")
+    
+    elif algo == b"CS":
+        print("Unsupported very OLD algorithm: "+algo_bytes+" , skipping...")
     else:
         print("Unsupported algorithm: "+algo_bytes+" , skipping...")
 
