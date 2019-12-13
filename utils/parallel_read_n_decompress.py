1d6c128d7f41e0ba5c8fb9ec94b21958709cf9d7
Diego Davila <diego.davila@cern.ch>
2019-12-14 00:19:27 +0100

    adding working version of parallel_read_n_decompress

--- utils/parallel_read_n_decompress.py
+++ utils/parallel_read_n_decompress.py
@@ -1,21 +1,22 @@
 import sys
 import os
 import zlib
-from multiprocessing import Process, Value, Array, Lock
+from multiprocessing import Process, Value, Lock
 
 
-def check_baskets(file_bytes, baskets_list, shrd_basket_index, chunk, num_baskets, shrd_corrupted, lock):
+def check_baskets(baskets_list, shrd_basket_index, chunk, num_baskets, shrd_corrupted, lock, filename):
     print("process : "+str(os.getpid())+ " starting check_baskets")
+    fd = open(filename)
     finished = False
     corrupted = False
     while(finished == False and corrupted==False):
         lock.acquire()
 
-        print("process : "+str(os.getpid())+ " checking")
+        #print("process : "+str(os.getpid())+ " checking")
         # If somebody else found something corrupted or there are no more baskets to analyze
         if shrd_corrupted.value == 1 or shrd_basket_index.value >= num_baskets:
             print("process : "+str(os.getpid())+ " finishing here. shrd_corrupted = "+str(shrd_corrupted.value)+" shrd_basket_index= "+str(shrd_basket_index.value))
-            print("process : "+str(os.getpid())+ " num_baskets = "+str(num_baskets))
+            #print("process : "+str(os.getpid())+ " num_baskets = "+str(num_baskets))
             finished = True
             lock.release()
             break
@@ -28,7 +29,9 @@ def check_baskets(file_bytes, baskets_list, shrd_basket_index, chunk, num_basket
         if stop > num_baskets:
             stop = num_baskets
         for seek in baskets_list[start:stop]:
-            header = file_bytes[seek-9:seek]
+            fd.seek(seek-9)
+            header = fd.read(9)
+            #header = file_bytes[seek-9:seek]
             algo_bytes = header[0:2]
             c1 = int(ord(header[3]))
             c2 = int(ord(header[4]))
@@ -44,7 +47,9 @@ def check_baskets(file_bytes, baskets_list, shrd_basket_index, chunk, num_basket
 
             if algo_bytes == "ZL":
                 try:
-                    zlib.decompress(file_bytes[seek:seek+num_compressed_bytes])
+                    fd.seek(seek)
+                    zlib.decompress(fd.read(num_compressed_bytes))
+                    #zlib.decompress(file_bytes[seek:seek+num_compressed_bytes])
                 except Exception, e:
                     corrupted = True
                     lock.acquire()
@@ -55,13 +60,13 @@ def check_baskets(file_bytes, baskets_list, shrd_basket_index, chunk, num_basket
             else:
                 print("Unsupported algorithm. skipping...")
 
+    fd.close()
 
 if __name__ == '__main__':
     file_root           = sys.argv[1]
     file_bl             = sys.argv[2]
-    file_size_root      = int(sys.argv[3])
+    num_processes       = int(sys.argv[3])
     
-    root_fd = open(file_root)
     bl_fd = open(file_bl)
 
     baskets = []
@@ -71,19 +76,19 @@ if __name__ == '__main__':
     chunk = 10
     basket_index = Value('i', 0)
     corrupted_flag = Value('i', 0)
-    arr_root = Array('c', root_fd.read(file_size_root))
     lock = Lock()
 
-    p1 = Process(target=check_baskets, args=(arr_root, baskets, basket_index, chunk, len(baskets), corrupted_flag, lock))
-    p2 = Process(target=check_baskets, args=(arr_root, baskets, basket_index, chunk, len(baskets), corrupted_flag, lock))
-    p3 = Process(target=check_baskets, args=(arr_root, baskets, basket_index, chunk, len(baskets), corrupted_flag, lock))
-    p1.start()
-    p2.start()
-    p3.start()
-    print("process : "+str(os.getpid())+ " waiting on workers")
-    p1.join()
-    p2.join()
-    p3.join()
+    process_list = []
+    for p in range(0, num_processes):
+        p = Process(target=check_baskets, args=( baskets, basket_index, chunk, len(baskets), corrupted_flag, lock, file_root))
+        p.start()
+        process_list.append(p)
+
+    check_baskets(baskets, basket_index, chunk, len(baskets), corrupted_flag, lock, file_root)  
+    
+    for p in process_list:
+        p.join()
+
     if corrupted_flag.value ==True:
         print("File is corrupted")
     else:
