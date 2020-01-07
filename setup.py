186b898141f4d7f53e53a228fd4fc761c8fb88d6
root <root@fermicloud168.fnal.gov>
2020-01-07 16:11:16 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -133,9 +133,8 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-                'zlib',
-                #'lzma',
-                'backports.lzma',
+                'numpy',
+                'uproot',
                 'xxhash',
     ],  # Optional
 
